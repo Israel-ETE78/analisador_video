df986cd495920a494a9dd2ff06716422af56c74d
Israel Paz <israelnetmax@gmail.com>
2025-08-06 10:28:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -384,6 +384,15 @@ def main_app():
                         'noplaylist': True,
                         'quiet': True,
                         'no_warnings': True,
+                        'http_headers': {
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                            'Accept-Language': 'en-US,en;q=0.5',
+                            'Referer': 'https://www.google.com/',
+                            'DNT': '1',
+                            'Connection': 'keep-alive',
+                            'Upgrade-Insecure-Requests': '1'
+                        }
                     }
                     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                         info = ydl.extract_info(video_link, download=True)
